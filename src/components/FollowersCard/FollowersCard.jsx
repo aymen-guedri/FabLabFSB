c6270c5b644c603acfd118303e19e0e3b7d55678
aymen guedri <guedri@fsb.u-carthage.tn>
2023-04-02 05:43:24 +0100

    first commit

---
+++ src/components/FollowersCard/FollowersCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FollowersCard() {
+  return (
+    <div></div>
+  )
+}
+
+export default FollowersCard
---
+++ src/components/ProfileCard/ProfileCard.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import "./ProfileCard.css";
+import Cover from "../../img/cover.jpg";
+import Profile from "../../img/profileImg.jpg";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+const ProfileCard = ({location}) => {
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const posts = useSelector((state)=>state.postReducer.posts)
+  const serverPublic = process.env.REACT_APP_PUBLIC_FOLDER;
+
+  return (
+    <div className="ProfileCard">
+      <div className="ProfileImages">
+        <img src={
+            user.coverPicture
+              ? serverPublic + user.coverPicture
+              : serverPublic + "defaultCover.jpg"
+          } alt="CoverImage" />
+        <img
+          src={
+            user.profilePicture
+              ? serverPublic + user.profilePicture
+              : serverPublic + "defaultProfile.png"
+          }
+          alt="ProfileImage"
+        />
+      </div>
+      <div className="ProfileName">
+        <span>{user.firstname} {user.lastname}</span>
+        <span>{user.worksAt? user.worksAt : 'FabLab FSB User'}</span>
+      </div>
+
+      <div className="followStatus">
+        <hr />
+        <div>
+          <div className="follow">
+            
+            <span>Edit Profile</span>
+          </div>
+          <div className="vl"></div>
+          <div className="follow">
+            
+            <span>my orders</span>
+          </div>
+          {/* for profilepage */}
+          {location === "profilePage" && (
+            <>
+              <div className="vl"></div>
+              <div className="follow">
+                
+                <span>WorkShops</span>
+              </div>{" "}
+            </>
+          )}
+        </div>
+        <hr />
+      </div>
+
+      {location === "profilePage" ? (
+        ""
+      ) : (
+        <span>
+          <Link to={`/profile/${user._id}`} style={{ textDecoration: "none", color: "inherit" }}>
+            My Profile
+          </Link>
+        </span>
+      )}
+    </div>
+  );
+};
+
+export default ProfileCard;
